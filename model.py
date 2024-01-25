41dd59aacf11d84c2d3a42f2c59dae3253bb5e99
Aries-Amit <157285804+Aries-Amit@users.noreply.github.com>
2024-01-25 17:53:31 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,6 @@
+# importing the required frameworks
+from keras.models import load_model
+
+
+# Loading the model
+models = load_model(r"C:\Users\amit9\/Project/Stock Price prediction Model\stock_prediction.pkl")
